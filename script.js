5c354aca2f2c5ea5e3148d9793a25ebfc90ae74f
Kyled4889 <73479292+Kyled4889@users.noreply.github.com>
2020-10-26 09:52:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Kyle"
+let userName = "Kyled4889";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
